cb7f034e7a3152fe2e09be754d498c6379eb200c
vinhvuu <vinhmaxdz192005@gmail.com>
2026-06-18 01:38:48 +0700

    Enhance ConvertModule with controllers and services

--- server/src/modules/sales/convert/convert.module.ts
+++ server/src/modules/sales/convert/convert.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
 
-@Module({})
+import { ConvertController } from './convert.controller';
+import { ConvertService } from './convert.service';
+
+import { DealsModule } from '../deals/deals.module';
+
+@Module({
+  imports: [DealsModule],
+  controllers: [ConvertController],
+  providers: [ConvertService],
+  exports: [ConvertService],
+})
 export class ConvertModule {}
