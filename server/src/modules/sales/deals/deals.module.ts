a3d5161bf11e7b3f9a8088016e3bdf98140b9439
vinhvuu <vinhmaxdz192005@gmail.com>
2026-06-18 01:41:00 +0700

    Enhance DealsModule with TypeOrm and controllers

--- server/src/modules/sales/deals/deals.module.ts
+++ server/src/modules/sales/deals/deals.module.ts
@@ -1,4 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+import { DealsController } from './deals.controller';
+import { DealsService } from './deals.service';
+import { SalesDeal } from '../database/entities/deal.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      SalesDeal,
+    ]),
+  ],
+  controllers: [
+    DealsController,
+  ],
+  providers: [
+    DealsService,
+  ],
+  exports: [
+    DealsService,
+  ],
+})
 export class DealsModule {}
