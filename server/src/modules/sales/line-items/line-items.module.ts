ea154d4e78b1769f35a2322a0f6813ce5802ea5e
vinhvuu <vinhmaxdz192005@gmail.com>
2026-06-18 01:44:03 +0700

    Update line-items.module.ts

--- server/src/modules/sales/line-items/line-items.module.ts
+++ server/src/modules/sales/line-items/line-items.module.ts
@@ -1,4 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+import { LineItemsController } from './line-items.controller';
+import { LineItemsService } from './line-items.service';
+import { SalesDealLineItem } from '../database/entities/deal-line-item.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      SalesDealLineItem,
+    ]),
+  ],
+  controllers: [
+    LineItemsController,
+  ],
+  providers: [
+    LineItemsService,
+  ],
+  exports: [
+    LineItemsService,
+  ],
+})
 export class LineItemsModule {}
