20e2a21fd5ce0090545cbde47fae0cc73ee163c7
vinhvuu <vinhmaxdz192005@gmail.com>
2026-06-18 01:47:49 +0700

    Update loss-reasons.module.ts

--- server/src/modules/sales/loss-reasons/loss-reasons.module.ts
+++ server/src/modules/sales/loss-reasons/loss-reasons.module.ts
@@ -1,4 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+import { LossReasonsController } from './loss-reasons.controller';
+import { LossReasonsService } from './loss-reasons.service';
+import { SalesLossReason } from '../database/entities/loss-reason.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      SalesLossReason,
+    ]),
+  ],
+  controllers: [
+    LossReasonsController,
+  ],
+  providers: [
+    LossReasonsService,
+  ],
+  exports: [
+    LossReasonsService,
+  ],
+})
 export class LossReasonsModule {}
