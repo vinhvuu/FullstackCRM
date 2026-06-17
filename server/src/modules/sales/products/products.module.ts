268aef2ded90fab88a85d4b2494074ba4ae87b49
vinhvuu <vinhmaxdz192005@gmail.com>
2026-06-18 01:50:44 +0700

    Update products.module.ts

--- server/src/modules/sales/products/products.module.ts
+++ server/src/modules/sales/products/products.module.ts
@@ -1,4 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+import { ProductsController } from './products.controller';
+import { ProductsService } from './products.service';
+import { SalesProduct } from '../database/entities/product.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      SalesProduct,
+    ]),
+  ],
+  controllers: [
+    ProductsController,
+  ],
+  providers: [
+    ProductsService,
+  ],
+  exports: [
+    ProductsService,
+  ],
+})
 export class ProductsModule {}
