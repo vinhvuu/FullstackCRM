6885344d22d4afb40dfc9f5e9037be56b54945de
vinhvuu <vinhmaxdz192005@gmail.com>
2026-06-18 01:49:36 +0700

    Update pipelines.module.ts

--- server/src/modules/sales/pipelines/pipelines.module.ts
+++ server/src/modules/sales/pipelines/pipelines.module.ts
@@ -1,4 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+import { PipelinesController } from './pipelines.controller';
+import { PipelinesService } from './pipelines.service';
+import { SalesPipeline } from '../database/entities/pipeline.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      SalesPipeline,
+    ]),
+  ],
+  controllers: [
+    PipelinesController,
+  ],
+  providers: [
+    PipelinesService,
+  ],
+  exports: [
+    PipelinesService,
+  ],
+})
 export class PipelinesModule {}
